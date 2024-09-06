a17b20ba9dee6478ccfa426bf7965f3956e3c9bb
petranih <gokilfitra528@gmail.com>
2024-09-06 16:06:24 +0700

    edit in sidebar and siswa index

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,7 +3,7 @@
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
       <div class="sidebar-brand-icon rotate-n-15">
-        </div>
+      </div>
       <div class="sidebar-brand-text mx-3">SMA 3 Sumenep</div>
     </a>
 
@@ -11,63 +11,63 @@
     <hr class="sidebar-divider my-0">
 
     <!-- Nav Item - Dashboard -->
-    <li class="nav-item">
+    <li class="nav-item {{ Request::is('dashboard') ? 'active' : '' }}">
       <a class="nav-link" href="{{ route('dashboard') }}">
         <i class="fas fa-fw fa-tachometer-alt"></i>
         <span>Dashboard</span></a>
     </li>
 
-    <li class="nav-item">
+    <li class="nav-item {{ Request::is('profile') ? 'active' : '' }}">
       <a class="nav-link" href="/profile">
-        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <i class="fas fa-fw fa-user"></i>
         <span>Profile</span></a>
     </li>
 
-    <li class="nav-item">
-        <a class="nav-link" href="/kelas">
-          <i class="fas fa-fw fa-tachometer-alt"></i>
-          <span>Kelas</span></a>
+    <li class="nav-item {{ Request::is('kelas') ? 'active' : '' }}">
+      <a class="nav-link" href="/kelas">
+        <i class="fas fa-fw fa-school"></i>
+        <span>Kelas</span></a>
     </li>
 
-    <li class="nav-item">
+    <li class="nav-item {{ Request::is('siswa') ? 'active' : '' }}">
       <a class="nav-link" href="/siswa">
-        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <i class="fas fa-fw fa-user-graduate"></i>
         <span>Siswa</span></a>
     </li>
 
-    <li class="nav-item">
+    <li class="nav-item {{ Request::is('absen') ? 'active' : '' }}">
       <a class="nav-link" href="/absen">
-        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <i class="fas fa-fw fa-calendar-check"></i>
         <span>Absensi</span></a>
     </li>
 
-    <li class="nav-item">
+    <li class="nav-item {{ Request::is('keterangan') ? 'active' : '' }}">
       <a class="nav-link" href="/keterangan">
-        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <i class="fas fa-fw fa-info-circle"></i>
         <span>Keterangan</span></a>
     </li>
 
-    <li class="nav-item">
+    <li class="nav-item {{ Request::is('rekap') ? 'active' : '' }}">
       <a class="nav-link" href="/rekap">
-        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <i class="fas fa-fw fa-file-alt"></i>
         <span>Rekap</span></a>
     </li>
 
-    <li class="nav-item">
+    <li class="nav-item {{ Request::is('ranking') ? 'active' : '' }}">
       <a class="nav-link" href="/ranking">
-        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <i class="fas fa-fw fa-sort-numeric-up"></i>
         <span>Ranking</span></a>
     </li>
 
-    <li class="nav-item">
+    <li class="nav-item {{ Request::is('scan') ? 'active' : '' }}">
       <a class="nav-link" href="/scan">
-        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <i class="fas fa-fw fa-qrcode"></i>
         <span>Scan</span></a>
     </li>
 
-    <li class="nav-item">
+    <li class="nav-item {{ Request::is('qr-code') ? 'active' : '' }}">
       <a class="nav-link" href="/qr-code">
-        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <i class="fas fa-fw fa-qrcode"></i>
         <span>QR Generate</span></a>
     </li>
 
@@ -79,5 +79,4 @@
       <button class="rounded-circle border-0" id="sidebarToggle"></button>
     </div>
 
-
-  </ul>
+</ul>
